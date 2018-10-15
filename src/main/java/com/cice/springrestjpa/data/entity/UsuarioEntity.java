e175d9bdaa7e1b4ec48df9aae2ec68c00005e7d9
djfroko <djfroko@gmail.com>
2018-10-15 20:05:01 +0200

    spring

--- src/main/java/com/cice/springrestjpa/data/entity/UsuarioEntity.java
+++ src/main/java/com/cice/springrestjpa/data/entity/UsuarioEntity.java
@@ -11,12 +11,45 @@ public class UsuarioEntity {
     @GeneratedValue
     @Column(name="id")
     private Long idUsuario;
-    
+
     @Column(name = "username")
     private String user;
 
     @Column(name="password")
     private String pass;
 
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
 
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public UsuarioEntity(Long idUsuario) {
+        this.idUsuario = idUsuario;
+
+    }
+
+    public UsuarioEntity(Long idUsuario, String user, String pass) {
+        this.idUsuario = idUsuario;
+        this.user = user;
+        this.pass = pass;
+    }
 }
